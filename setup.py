57aec3d470f04fe768eb1c39e29a80b6f74e0da9
RJSINGH64 <rajat.k.singh64@gmail.com>
2024-08-07 17:27:22 +0530

    updated setup files

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import find_packages , setup
 requirements_file = "requirements.txt"
 hyphen_e_dot = "-e ."
 
-def get_requirements()-->list[str]:
+def get_requirements()->list[str]:
    with open (requirements_file) as requirement_file :
       requirement_list = requirement_file.readlines()
-      requirement_list = [requirements_name.replace("\n" , "") for requirements_name in requiremencd t_list ]
+      requirement_list = [requirements_name.replace("\n" , "") for requirements_name in requirement_list]
       
-    if hyphen_edot in requirement_list :
-       requirement_list.remove(hyphen_edot)
-    return requirement_list
+   if hyphen_e_dot in requirement_list :
+       requirement_list.remove(hyphen_e_dot)
+   return requirement_list
      
     
 
@@ -21,9 +21,9 @@ def get_requirements()-->list[str]:
 setup (
 
     name = "sensor" , 
-    version = "0.0.2" , 
+    version = "0.0.1" , 
     author= "iNeuron.ai" , 
-    author_email=rajat.k.singh64@gmail.com , 
+    author_email="rajat.k.singh64@gmail.com" , 
     packages=  find_packages() , 
     install_requires = get_requirements() , 
 
