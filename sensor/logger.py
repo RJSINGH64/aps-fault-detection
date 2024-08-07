8f0a0316c09dff0b941c3216e8e9be985016f250
RJSINGH64 <rajat.k.singh64@gmail.com>
2024-08-07 19:04:53 +0530

    logging

---
+++ sensor/logger.py
@@ -0,0 +1,22 @@
+import os
+import logging 
+from datetime import datetime
+#log file
+log_file_name = f"{datetime.now().strftime("%m%d%y__%H%M%S")}.log"
+
+#log directory
+log_directory=os.path.join(os.getcwdb() , "logs")
+
+#create directory if not available
+os.makedirs(log_directory, exist_ok=True)
+
+#log file path 
+log_file_path=os.path.join(log_directory ,  log_file_name)
+
+logging.basicConfig( 
+
+    filename= log_file_path , 
+    format='[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s " , 
+    level=logging.INFO , 
+
+)
