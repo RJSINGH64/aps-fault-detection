48b6d2c730a82bf49cd168e051145c41ed322a49
RJSINGH64 <rajat.k.singh64@gmail.com>
2024-09-04 20:59:06 +0530

    updated Model Evaluation

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+
+# **First use conda environment python==3.8 for this project**
+
 # neurolab-mongo-python
 
 ![image](https://user-images.githubusercontent.com/57321948/196933065-4b16c235-f3b9-4391-9cfe-4affcec87c35.png)
--- main.py
+++ main.py
@@ -21,22 +21,22 @@ if __name__=="__main__":
         print(data_ingestion_config.to_dict()) 
         data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_config)
         data_ingestion_artifact=data_ingestion.initiate_data_ingestion()
-        print("1st pipeline data ingestion initiated  Sucessfully")
+        print("\n==>"*5,"1st pipeline data ingestion initiated  Sucessfully","<=="*5)
         #Data validation 
         data_validation_config  = DataValidationConfig(training_pipe_config)
         data_validation = DataValidation(data_validation_config=data_validation_config , data_ingestion_artifact=data_ingestion_artifact)
         data_validation_artifact=data_validation.initiate_data_validation()
-        print(" 2nd Pipeline Data validation initiated  Sucessfully")
+        print("==>"*5,"2nd Pipeline Data validation initiated  Sucessfully","<=="*5)
         #Data transformation
         data_transformation_config = DataTransformationConfig(training_pipe_config)
         data_transformation=DataTransformation(data_transformation_config=data_transformation_config , data_ingestion_artifact=data_ingestion_artifact)
         data_transformation_artifact = data_transformation.initiate_data_transformation()
-        print("3rd Pipeline Data transformation initiated Sucessfully")
+        print("==>"*5,"3rd Pipeline Data transformation initiated Sucessfully","<=="*5)
         #model trainer
         model_trainer_config = ModelTrainerConfig(training_pipe_config)
         model_trainer = ModelTrainer(model_trainer_config=model_trainer_config , data_transformation_artifact=data_transformation_artifact)
         model_trainer_artifact = model_trainer.initiate_model_trainer()
-        print("4th Model trainer Initiated Sucessfully")
+        print("==>"*5,"4th Model trainer Initiated Sucessfully","<=="*5)
         #model evaluation
 
         model_eva_config = ModelEvaluationConfig(training_pipeline_config=training_pipe_config)
@@ -47,7 +47,7 @@ if __name__=="__main__":
                                           )
                                           
         model_evalution_artifact = model_evalution.initiate_model_evaluation()    
-        print(" 5th Model Evalution Initiated Sucessfully")                             
+        print("==>"*5 , " 5th Pipeline Model Evalution Initiated Sucessfully" , "<=="*5)                             
 
 
     except Exception as e:
